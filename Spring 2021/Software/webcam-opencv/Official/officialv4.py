9e4856ebc6fc5ceac03087cf12450d2e3938197b
chrisclai <chrisclai02@gmail.com>
2021-04-22 18:39:28 -0700

    code refactoring, getting toggles to work

--- Spring 2021/Software/webcam-opencv/Official/officialv4.py
+++ Spring 2021/Software/webcam-opencv/Official/officialv4.py
@@ -1,5 +1,6 @@
-# Official script official.py
+# Official script [officialv4.py]
 
+# Import Libraries
 import numpy as np
 import cv2
 import os
@@ -13,7 +14,8 @@ import socket
 import tkinter as tk
 import random
 import math
-#from PIL import Image, ImageTk
+
+from PIL import Image, ImageTk
 
 from _thread import *
 import threading
@@ -27,31 +29,49 @@ global pY
 global dX
 global dY
 
-global isScFrame
-global isScMask
-global isPointDetecting
-global isState
-global isBye
-global isBye2
-global isBye3
+# Default Variables for Color
+l_h = 0 
+l_s = 38
+l_v = 132
+u_h = 255
+u_s = 255
+u_v = 255
+l_range_hsv = np.array([l_h, l_s, l_v])
+u_range_hsv = np.array([u_h, u_s, u_v])
 
-# DEFAULT CONSTRUCTORS
+# Default Variables for Position
 pX = 0
 pY = 0
 dX = 0
 dY = 0
 
-isScFrame = False
-isScMask = False
-isPointDetecting = False
-isState = False
+# Logic Variables
+global isFrameBtnPressed
+isFrameBtnPressed = False
+isFrameOpen = False
+
+global isMaskBtnPressed
+isMaskBtnPressed = False
+isMaskOpen = False
+
+global isMAlgBtnPressed
+isMAlgBtnPressed = False
+maskAlg = 0 # Adaptive Threshold Alg by default
+
+global isContourBtnPressed
+isContourBtnPressed = True
+isContourShowing = False
+
+global isRandBtnPressed
+isRandBtnPressed = False
 isTargetReached = True
-isBye = True
-isBye2 = True
-isBye3 = True
+
+global isTBarBtnPressed
+isTBarBtnPressed = False
+isTBarOpen = False
 firstTimeTrackbar = True
 
-def nothing(x):
+def nothing(f):
     pass
 
 def saveFile(image):
@@ -77,77 +97,69 @@ def tkinter():
     global dX
     global dY
 
+    # ICONS (Courtesy of Icons8.com)
+    camera_icon = tk.PhotoImage(file='icons\\camera.png')
+    frame_icon = tk.PhotoImage(file='icons\\frame.png')
+    mask_icon = tk.PhotoImage(file='icons\\switch-maskalg.png')
+    contour_icon = tk.PhotoImage(file='icons\\contour.png')
+    target_icon = tk.PhotoImage(file='icons\\rand-point.png')
+    trackbar_icon = tk.PhotoImage(file='icons\\trackbar.png')
+    workflow_icon = tk.PhotoImage(file='icons\\testrun.png')
+
     # CANVAS
     control_canv = tk.Canvas(main_canv, width=240, height=800, highlightthickness=0, bg='black')   
     control_canv.place(x=60, y=60, anchor='nw') 
 
     # LABEL
-    title = tk.Label(control_canv, text="ROBOTIS", font=('courier new',24,'bold italic'), justify='center', bg='black', fg='#f46500')
-    title.place(relx=0.5,rely=0.05,anchor='center')
+    title = tk.Label(main_canv, text="ROBOTIS", font=('courier new',24,'bold italic'), justify='center', bg='black', fg='#FF8242')
+    title.place(relx=0.5,rely=0.085,anchor='center')
 
-    subtitle = tk.Label(control_canv, text="Remote Control", font=('courier new',20,'bold'), justify='center', bg='black', fg='#4d7701')
-    subtitle.place(relx=0.5,rely=0.1,anchor='center')
-
-    # point = tk.Label(control_canv, text="Point: ({}, {})".format(pX, pY), font=('courier new',14,'bold'), justify='left', bg='black', fg='#f4f0f4')
-    # point.place(relx=0.3,rely=0.65,anchor='center')
-
-    # distanceX = tk.Label(control_canv, text="dX: {}".format(dX), font=('courier new',14,'bold'), justify='left', bg='black', fg='#f4f0f4')
-    # distanceX.place(relx=0.115,rely=0.7,anchor='center')
-
-    # distanceY = tk.Label(control_canv, text="dY: {}".format(dY), font=('courier new',14,'bold'), justify='left', bg='black', fg='#f4f0f4')
-    # distanceY.place(relx=0.115,rely=0.75,anchor='center')
+    subtitle = tk.Label(main_canv, text="Remote Control", font=('courier new',20,'bold'), justify='center', bg='black', fg='#914FA6')
+    subtitle.place(relx=0.5,rely=0.125,anchor='center')
 
     # BUTTON FUNCTIONS
-    def scFrame():
-        global isScFrame
-        isScFrame = True
+    def toggleFrame():
+        global isFrameBtnPressed
+        isFrameBtnPressed = True
 
-    def scMask():
-        global isScMask
-        isScMask = True
+    def toggleMask():
+        global isMaskBtnPressed
+        isMaskBtnPressed = True
         
+    def toggleMaskAlg():
+        global isMAlgBtnPressed
+        isMAlgBtnPressed = True
+
+    def toggleContour():
+        global isContourBtnPressed
+        isContourBtnPressed = True
+
+    def toggleTrackbar():
+        global isTBarBtnPressed
+        isTBarBtnPressed = True
+
     def rndPoint():
-        global isPointDetecting
-        isPointDetecting = True
-
-    def changeState():
-        global isState
-        isState = True
-    
-    # OFF BUTTONS
-    def bye():
-        global isBye
-        isBye=False
-
-    def bye2():
-        global isBye2
-        isBye2=False
-
-    def bye3():
-        global isBye3
-        isBye3=False
+        global isRandBtnPressed
+        isRandBtnPressed = True
 
     # BUTTONS
-    screenshot_frame = tk.Button(control_canv, text="sc frame", font=('courier new',18,'bold'), command=scFrame, justify='center', padx=40, pady=10, bg='black', fg='#9e8d8f')
-    screenshot_frame.place(relx=0.5,rely=0.2,anchor='center')
-
-    close_frame=tk.Button(control_canv, text="close frame", font=('courier new',18,'bold'), command=bye, justify='center', padx=20, pady=5, bg='black', fg='#9e8d8f')
-    close_frame.place(relx=0.5,rely=0.275,anchor='center')
+    display_frame = tk.Button(control_canv, image = camera_icon, command=toggleFrame, justify='center', padx=10, pady=10, bg='black', fg='#9e8d8f')
+    display_frame.place(relx=0.5,rely=0.167,anchor='center')
     
-    screenshot_mask = tk.Button(control_canv, text="sc mask", font=('courier new',18,'bold'), command=scMask, justify='center', padx=40, pady=10, bg='black', fg='#9e8d8f')
-    screenshot_mask.place(relx=0.5,rely=0.4,anchor='center')
+    display_mask = tk.Button(control_canv, image = frame_icon, command=toggleMask, justify='center', padx=40, pady=10, bg='black', fg='#9e8d8f')
+    display_mask.place(relx=0.5,rely=0.333,anchor='center')
 
-    close_mask=tk.Button(control_canv, text="close mask", font=('courier new',18,'bold'), command=bye2, justify='center', padx=20, pady=5, bg='black', fg='#9e8d8f')
-    close_mask.place(relx=0.5,rely=0.475,anchor='center')
+    change_maskalg = tk.Button(control_canv, image = mask_icon, command=toggleMaskAlg, justify='center', padx=40, pady=10, bg='black', fg='#9e8d8f')
+    change_maskalg.place(relx=0.3,rely=0.5,anchor='center')
 
-    change_state = tk.Button(control_canv, text="--state--", font=('courier new',18,'bold'), command=changeState, justify='center', padx=40, pady=10, bg='black', fg='#9e8d8f')
-    change_state.place(relx=0.5,rely=0.6,anchor='center')
+    display_contour = tk.Button(control_canv, image = contour_icon, command=toggleContour, justify='center', padx=40, pady=10, bg='black', fg='#9e8d8f')
+    display_contour.place(relx=0.7,rely=0.5,anchor='center')
 
-    close_state=tk.Button(control_canv, text="close state", font=('courier new',18,'bold'), command=bye3, justify='center', padx=20, pady=5, bg='black', fg='#9e8d8f')
-    close_state.place(relx=0.5,rely=0.675,anchor='center')
+    display_trackbar = tk.Button(control_canv, image = trackbar_icon, command=toggleTrackbar, justify='center', padx=40, pady=10, bg='black', fg='#9e8d8f')
+    display_trackbar.place(relx=0.5,rely=0.667,anchor='center')
     
-    rand_point = tk.Button(control_canv, text="rand point", font=('courier new',18,'bold'), command=rndPoint, justify='center', padx=40, pady=10, bg='black', fg='#9e8d8f')
-    rand_point.place(relx=0.5,rely=0.8,anchor='center')
+    rand_point = tk.Button(control_canv, image = target_icon, command=rndPoint, justify='center', padx=40, pady=10, bg='black', fg='#9e8d8f')
+    rand_point.place(relx=0.5,rely=0.833,anchor='center')
 
     exitButton = tk.Button(control_canv, text="EXIT", font=('courier new',18,'bold'), command=exit, justify='center', padx=40, pady=10, bg='black', fg='red')
     exitButton.place(relx=0.5,rely=.95,anchor='center')
@@ -182,111 +194,28 @@ while(True):
     # Apply Gaussian Blur
     blur = cv2.GaussianBlur(grayscale, (9,9), 0)
 
-    # Create Mask/Threshold
-    threshold = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 23, 3)
-
-    # Find contours
-    contours = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = imutils.grab_contours(contours)
-    
-    # Test
-    cv2.imshow('test', frame)
-
-    # stop fps counter and calculate + show fps
-    stop = time.time()
-
-    seconds = stop - start
-
-    # Calculate frames per second
-    fps = 1 / seconds
-
-    # Fps display
-    cv2.putText(frame,"FPS: " + str(round(fps,2)), (25,40) ,cv2.FONT_HERSHEY_SIMPLEX, 0.75,(50,200,50),2)
-
-    #swapping into the state
-    if isState:
-        if firstTimeTrackbar:
-            # Create a window named trackbars.
-            cv2.namedWindow("Trackbars")
-
-            # Now create 6 trackbars that will control the lower and upper range of 
-            # H,S and V channels. The Arguments are like this: Name of trackbar, 
-            # window name, range,callback function. For Hue the range is 0-179 and
-            # for S,V its 0-255.
-            cv2.createTrackbar("L - H", "Trackbars", 0, 179, nothing)
-            cv2.createTrackbar("L - S", "Trackbars", 0, 255, nothing)
-            cv2.createTrackbar("L - V", "Trackbars", 0, 255, nothing)
-            cv2.createTrackbar("U - H", "Trackbars", 179, 179, nothing)
-            cv2.createTrackbar("U - S", "Trackbars", 255, 255, nothing)
-            cv2.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
-            firstTimeTrackbar = False
- 
-        # Convert the BGR image to HSV image.
+    # Decide on which mask algorithm to use based on button [0 = AdaptiveThreshold, 1 = HSV Color Isolation]
+    if isMAlgBtnPressed:
+        if maskAlg == 0: maskAlg = 1 
+        else: maskAlg = 0 
+        isMAlgBtnPressed = False
+    if maskAlg == 0:
+        mask = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 23, 3)
+    else:
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        
-        # Get the new values of the trackbar in real time as the user changes 
-        # them
-        l_h = cv2.getTrackbarPos("L - H", "Trackbars")
-        l_s = cv2.getTrackbarPos("L - S", "Trackbars")
-        l_v = cv2.getTrackbarPos("L - V", "Trackbars")
-        u_h = cv2.getTrackbarPos("U - H", "Trackbars")
-        u_s = cv2.getTrackbarPos("U - S", "Trackbars")
-        u_v = cv2.getTrackbarPos("U - V", "Trackbars")
+        mask = cv2.inRange(hsv, l_range_hsv, u_range_hsv)
+
+    # Run contour detection and display if toggled
+    if isContourBtnPressed:
+        if not isContourShowing:
+            isContourShowing = True
+        else:
+            isContourShowing = False
+        isContourBtnPressed = False
     
-        # Set the lower and upper HSV range according to the value selected
-        # by the trackbar
-        lower_range = np.array([l_h, l_s, l_v])
-        upper_range = np.array([u_h, u_s, u_v])
-        
-        # Filter the image and get the binary mask, where white represents 
-        # your target color
-        mask = cv2.inRange(hsv, lower_range, upper_range)
-    
-        # You can also visualize the real part of the target color (Optional)
-        res = cv2.bitwise_and(frame, frame, mask=mask)
-        
-        # Converting the binary mask to 3 channel image, this is just so 
-        # we can stack it with the others
-        mask_3 = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-        
-        # stack the mask, orginal frame and the filtered result
-        stacked = np.hstack((mask_3,frame,res))
-        
-        # Show this stacked frame at 40% of the size.
-        cv2.imshow('Trackbars',cv2.resize(stacked,None,fx=0.5,fy=0.5))
-    if isBye3==False:
-        print("Hit3")
-        isState = False
-        cv2.destroyWindow('Trackbars')
-        isBye3 = True
-        firstTimeTrackbar = True
-
-    # TKINTER-LINKED CONDITIONALS
-    if isPointDetecting == True:
-        data = {}
-        if not isTargetReached:
-            dX = pX - cX
-            dY = pY - cY
-            
-            cv2.circle(frame, (pX, pY), 7, (0,0,255), -1)
-            cv2.putText(frame, "Target", (pX - 23, pY - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
-            cv2.putText(frame, "Target Location: ({}, {})".format(pX, pY), (25, 450), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2)
-            
-            print("CONSOLE: \t dX = {} \t dY = {}".format(dX, dY))
-
-            if dX < 25 and dX > -25 and dY < 25 and dY > -25:
-                print("Target Successfully Aquired.")
-                isTargetReached = True
-                isPointDetecting = False
-                pX = 0
-                pY = 0     
-        elif isPointDetecting == True and isTargetReached == True:
-            pX = random.randint(0,640)
-            pY = random.randint(0,480)
-            isTargetReached = False
-
-    # Turning on and off the webcam
-    if isScFrame == True:
+    if isContourShowing:
+        contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours = imutils.grab_contours(contours)
         # Loop for all contours
         contnum = 0
         for c in contours:
@@ -304,26 +233,35 @@ while(True):
                 cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
                 cv2.circle(frame, (cX, cY), 7, (0,0,0), -1)
                 cv2.putText(frame, "center", (cX - 23, cY - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 2)
-                cv2.putText(frame, "Location: ({}, {})".format(cX, cY), (450, 450), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
-            
-            # Display number of contours detected
-            cv2.putText(frame, "# of contours: {}".format(contnum), (450, 425), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
-        
-        cv2.imshow('webcam', frame)
-    if isBye == False:
-        print("hit1")
-        isScFrame = False
-        cv2.destroyWindow('webcam')
-        isBye=True    
+                cv2.putText(frame, "Location: ({}, {})".format(cX, cY), (450, 450), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)        
+        # Display number of contours detected
+        cv2.putText(frame, "# of contours: {}".format(contnum), (450, 425), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
 
-    # Turning on and off the mask
-    if isScMask == True:
-        cv2.imshow('mask', threshold)
-    if isBye2 == False:
-        print("hit2")
-        isScMask = False
-        cv2.destroyWindow('mask')
-        isBye2=True     
+    # Stop fps counter, calculate, and show fps
+    stop = time.time()
+    seconds = stop - start
+    fps = 1 / seconds
+    cv2.putText(frame,"FPS: " + str(round(fps,2)), (25,40) ,cv2.FONT_HERSHEY_SIMPLEX, 0.75,(50,200,50),2)
+    
+    # Toggle Frame
+    if isFrameBtnPressed:
+        if not isFrameOpen:
+            isFrameOpen = True
+        else:
+            cv2.destroyWindow('Frame')
+            isFrameOpen = False
+        isFrameBtnPressed = False
+    if isFrameOpen: cv2.imshow('Frame', frame)
+
+    # Toggle Mask
+    if isMaskBtnPressed:
+        if not isMaskOpen:
+            isMaskOpen = True
+        else:
+            cv2.destroyWindow('Mask')
+            isMaskOpen = False
+        isMaskBtnPressed = False
+    if isMaskOpen: cv2.imshow('Mask', mask)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
