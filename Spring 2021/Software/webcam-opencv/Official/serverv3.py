bf8f1897a292cd97593d322ae6ecebaf884e0919
addsandwich <5283833+addsandwich@users.noreply.github.com>
2021-07-09 10:34:50 -0700

    FullScaleIntegration
    Fun

--- Spring 2021/Software/webcam-opencv/Official/serverv3.py
+++ Spring 2021/Software/webcam-opencv/Official/serverv3.py
@@ -53,6 +53,7 @@ cameraInput = {}
 #This is for the current arm status
 #1-Moving
 #2-Waiting
+#3-Error
 global armStatus 
 armStatus = {}
 
@@ -128,46 +129,48 @@ def read_async(connIn, addr):
     global newData
     global state
     while True:
-        # try:
-        data_recv = connIn.recv(BUFFER_SIZE).decode('utf-8')
-        data_recv = json.loads(data_recv)
-        print(f"Recieved: {data_recv}, attempting load...")
-        load_data(data_recv)
-        if newData:
-            armStatus["code"] = 1
-            print("Data loaded, initiating command.")
-            newData = False
-            if cameraInput["command"]==3:
-                print("Home command, processing.")
-                if state == 1:
+        try:
+            data_recv = connIn.recv(BUFFER_SIZE).decode('utf-8')
+            data_recv = json.loads(data_recv)
+            print(f"Recieved: {data_recv}, attempting load...")
+            load_data(data_recv)
+            if newData:
+                armStatus["code"] = 1
+                print("Data loaded, initiating command.")
+                newData = False
+                if cameraInput["command"]==3:
+                    print("Home command, processing.")
+                    if state == 1:
+                        print("Dropping load.")
+                        arm.drop_load()
+                    print("Going home.")
+                    arm.set_Location(home)
+                    state = 3
+                elif cameraInput["command"]==1:
+                    print("Move command, processing.")
+                    if state == 3:
+                        print("Grabbing load.")
+                        arm.grab_load()
+                    new_location=arm.translate_diff(cameraInput)
+                    arm.set_location(new_location)
+                    print("Going to differential location.")
+                    state = 1
+                elif cameraInput["command"]==2:
                     print("Dropping load.")
-                    #drop_load()
-                print("Going home.")
-                #set_Location(home)
-                state = 3
-            elif cameraInput["command"]==1:
-                print("Move command, processing.")
-                if state == 3:
+                    arm.drop_load()
+                    state=2 
+                elif cameraInput["command"]==4:
                     print("Grabbing load.")
-                    #grab_load()
-                #new_location=translate_diff(cameraInput)
-                #set_location(new_location)
-                print("Going to differential location.")
-                state = 1
-            elif cameraInput["command"]==2:
-                print("Dropping load.")
-                #drop_load()
-                state=2 
-            elif cameraInput["command"]==4:
-                print("Grabbing load.")
-                #grab_load()
-                state=4
-        armStatus["code"] = 2
-        connIn.sendall(json.dumps(armStatus).encode('utf-8')) # encode the dict to JSON
-        # except:
+                    arm.grab_load()
+                    state=4
+            armStatus["code"] = 2
+            connIn.sendall(json.dumps(armStatus).encode('utf-8')) # encode the dict to JSON
+        except:
+            armStatus["code"] = 3
+            connIn.sendall(json.dumps(armStatus).encode('utf-8')) # encode the dict to JSON
             # print(f"Packet receive attempt to {addr} failed. Closing connection.")
             # connIn.close()
-            # break
+            break
             
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT)) #Bind system socket
