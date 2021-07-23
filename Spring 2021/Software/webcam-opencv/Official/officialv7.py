3cb2165d11854a501fc5760256db386fd36b74c8
chrisclai <chrisclai02@gmail.com>
2021-07-22 23:12:27 -0700

    Update officialv7.py

--- Spring 2021/Software/webcam-opencv/Official/officialv7.py
+++ Spring 2021/Software/webcam-opencv/Official/officialv7.py
@@ -677,8 +677,8 @@ while(True):
         data = dict.fromkeys(keys)
         
         data['name'] = "Camera1"
-        data['dX'] = int(((cX - QcX) * MmtoPixelRatio)/1.6) # Converts pixels to mm (real life measurement)
-        data['dY'] = int(((QcY - cY) * MmtoPixelRatio)/1.5) # Converts pixels to mm (real life measurement)
+        data['dX'] = int(((cX - QcX) * MmtoPixelRatio)/2) # Converts pixels to mm (real life measurement)
+        data['dY'] = int(((QcY - cY) * MmtoPixelRatio)/2) # Converts pixels to mm (real life measurement)
         data['command'] = 1
 
         try:
